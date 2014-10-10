de1289947f285e572a40354b2775f9f43b9018b5
Daniel Bruce <dbruce@vectorface.com>
2014-10-10 11:52:25 -0400

    DiProvider is no longer a trait for PHP 5.3 compatibility.

--- src/VectorFace/SnappyRouter/Di/DiProvider.php
+++ src/VectorFace/SnappyRouter/Di/DiProvider.php
@@ -3,22 +3,27 @@
 namespace VectorFace\SnappyRouter\Di;
 
 /**
- * A trait that indicates the class provides the DI layer through the standard
- * expected methods.
+ * An interface that exposes the main DI container methods. Implemented by
+ * various elements of router for convenience.
  * @copyright Copyright (c) 2014, VectorFace, Inc.
  * @author Dan Bruce <dbruce@vectorface.com>
  */
-trait DiProvider
+interface DiProvider
 {
     /**
-     * Helper method to fetch DI dependencies.
+     * Retrieve an element from the DI container.
      * @param string $key The DI key.
      * @param boolean $useCache (optional) An optional indicating whether we
      *        should use the cached version of the element (true by default).
      * @return Returns the DI element mapped to that key.
      */
-    protected function get($key, $useCache = true)
-    {
-        return DI::getDefault()->get($key, $useCache);
-    }
+    protected function get($key, $useCache = true);
+
+    /**
+     * Sets an element in the DI container for the specified key.
+     * @param string $key The DI key.
+     * @param mixed  $element The DI element to store.
+     * @return Di Returns the Di instance.
+     */
+    public function set($key, $element);
 }
