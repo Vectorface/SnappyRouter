f07aa8629f405941bb35d59752ca917c1489abc1
Daniel Bruce <dbruce@vectorface.com>
2014-10-31 11:52:39 -0400

    New MethodNotAllowedException added.

---
+++ src/Vectorface/SnappyRouter/Exception/MethodNotAllowedException.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Vectorface\SnappyRouter\Exception;
+
+use \Exception;
+use Vectorface\SnappyRouter\Response\AbstractResponse;
+
+/**
+ * An exception indicating an invalid HTTP action was specified.
+ * @copyright Copyright (c) 2014, VectorFace, Inc.
+ * @author Dan Bruce <dbruce@vectorface.com>
+ */
+class MethodNotAllowedException extends Exception implements RouterExceptionInterface
+{
+    // as per RFC2616 (http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.6)
+    // we must specify a comma separated list of allowed methods if we return
+    // a 405 response
+    private $allowedMethods;
+
+    /**
+     * Constructor for the method.
+     * @param string $message The error message string.
+     * @param array $allowedMethods The array of methods that are allowed.
+     */
+    public function __construct($message, $allowedMethods)
+    {
+        parent::__construct($message);
+        $this->allowedMethods = $allowedMethods;
+    }
+
+    /**
+     * Returns the associated status code with the exception. By default, most exceptions correspond
+     * to a server error (HTTP 500). Override this method if you want your exception to generate a
+     * different status code.
+     * @return The associated status code.
+     */
+    public function getAssociatedStatusCode()
+    {
+        if (!empty($this->allowedMethods) && is_array($this->allowedMethods)) {
+            @header(sprintf('Allow: %s', implode(',', $this->allowedMethods)));
+        }
+        return AbstractResponse::RESPONSE_METHOD_NOT_ALLOWED;
+    }
+}
---
+++ tests/Vectorface/SnappyRouterTests/Exception/MethodNotAllowedExceptionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Vectorface\SnappyRouterTests\Exception;
+
+use \PHPUnit_Framework_TestCase;
+use Vectorface\SnappyRouter\Exception\MethodNotAllowedException;
+
+/**
+ * Tests the MethodNotAllowedException class.
+ * @copyright Copyright (c) 2014, VectorFace, Inc.
+ * @author Dan Bruce <dbruce@vectorface.com>
+ */
+class MethodNotAllowedExceptionTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * An overview of how the class works.
+     * @test
+     */
+    public function synopsis()
+    {
+        $exception = new MethodNotAllowedException(
+            'Cannot use GET',
+            array('POST')
+        );
+        $this->assertEquals(405, $exception->getAssociatedStatusCode());
+        try {
+            throw $exception;
+        } catch (MethodNotAllowedException $e) {
+            $this->assertEquals('Cannot use GET', $e->getMessage());
+        }
+    }
+}
