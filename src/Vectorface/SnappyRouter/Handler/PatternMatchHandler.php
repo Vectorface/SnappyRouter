0cc1a4d9ce7615137c3bd19af8ddafb1b2a8ec61
Daniel Bruce <dbruce@vectorface.com>
2014-11-13 10:08:46 -0500

    Added pattern based handler based on nikic/FastRoute.

---
+++ src/Vectorface/SnappyRouter/Handler/PatternMatchHandler.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Vectorface\SnappyRouter\Handler;
+
+use FastRoute\Dispatcher;
+use FastRoute\RouteCollector;
+
+/**
+ * A handler for matching route patterns and mapping to a method.
+ * Internally the class uses FastRoute to do the pattern matching.
+ * @copyright Copyright (c) 2014, VectorFace, Inc.
+ * @author Dan Bruce <dbruce@vectorface.com>
+ */
+class PatternMatchHandler extends AbstractRequestHandler
+{
+    // the currently active callback
+    private $callback;
+    // the currently active route parameters
+    private $routeParams;
+
+    /** All supported HTTP verbs */
+    private static $allHttpVerbs = array(
+        'GET',
+        'POST',
+        'PUT',
+        'DELETE',
+        'OPTIONS'
+    );
+
+    /**
+     * Returns true if the handler determines it should handle this request and false otherwise.
+     * @param string $path The URL path for the request.
+     * @param array $query The query parameters.
+     * @param array $post The post data.
+     * @param string $verb The HTTP verb used in the request.
+     * @return Returns true if this handler will handle the request and false otherwise.
+     */
+    public function isAppropriate($path, $query, $post, $verb)
+    {
+        $options = $this->getOptions();
+        $dispatcher = $this->getDispatcher($options['routes']);
+        $routeInfo = $dispatcher->dispatch(strtoupper($verb), $path);
+        switch ($routeInfo[0]) {
+            case Dispatcher::FOUND:
+                break;
+            default:
+                return false;
+        }
+        $this->callback = $routeInfo[1];
+        $this->routeParams = isset($routeInfo[2]) ? $routeInfo[2] : array();
+        return true;
+    }
+
+    /**
+     * Performs the actual routing.
+     * @return Returns the result of the route.
+     */
+    public function performRoute()
+    {
+        return call_user_func($this->callback, $this->routeParams);
+    }
+
+    /**
+     * Returns an instance of the FastRoute dispatcher.
+     * @param array $routes The array of specified routes.
+     * @return FastRoute\Dispatcher The dispatcher to use.
+     */
+    private function getDispatcher($routes)
+    {
+        $verbs = self::$allHttpVerbs;
+        $f = function (RouteCollector $collector) use ($routes, $verbs) {
+            foreach ($routes as $pattern => $route) {
+                if (is_array($route)) {
+                    foreach ($route as $verb => $callback) {
+                        $collector->addRoute(strtoupper($verb), $pattern, $callback);
+                    }
+                } else {
+                    foreach ($verbs as $verb) {
+                        $collector->addRoute($verb, $pattern, $route);
+                    }
+                }
+            }
+        };
+        return \FastRoute\simpleDispatcher($f);
+    }
+}
---
+++ tests/Vectorface/SnappyRouterTests/Handler/PatternMatchHandlerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Vectorface\SnappyRouterTests\Handler;
+
+use \PHPUnit_Framework_TestCase;
+use Vectorface\SnappyRouter\Handler\PatternMatchHandler;
+
+/**
+ * A test for the PatternMatchHandler class.
+ * @copyright Copyright (c) 2014, VectorFace, Inc.
+ * @author Dan Bruce <dbruce@vectorface.com>
+ */
+class PatternMatchHandlerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Demonstrates how to use the PatternMatchHandler class.
+     * @test
+     */
+    public function synopsis()
+    {
+        $config = array(
+            'routes' => array(
+                '/user/{name}/{id:[0-9]+}' => array(
+                    'get' => function ($routeParams) {
+                        return print_r($routeParams, true);
+                    }
+                ),
+                '/anotherRoute' => function ($routeParams) {
+                    return false;
+                }
+            )
+        );
+        $handler = new PatternMatchHandler($config);
+        $this->assertTrue($handler->isAppropriate('/user/asdf/1234', array(), array(), 'GET'));
+        $expected = print_r(array('name' => 'asdf', 'id' => 1234), true);
+        $this->assertEquals($expected, $handler->performRoute());
+
+        // not a matching pattern
+        $this->assertFalse($handler->isAppropriate('/user/1234', array(), array(), 'GET'));
+
+        // matching pattern but invalid HTTP verb
+        $this->assertFalse($handler->isAppropriate('/user/asdf/1234', array(), array(), 'POST'));
+    }
+}
